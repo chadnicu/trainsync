b80c8c35a9b5aba741501ef038516c542f4df183
nicu420 <ontanicolae15@gmail.com>
2023-06-20 17:45:51 +0300

    bug fix

--- app/sessions/[id]/page.tsx
+++ app/sessions/[id]/page.tsx
@@ -2,34 +2,52 @@ import AddButton from "@/components/AddButtonn";
 import RemoveButton from "@/components/RemoveButton";
 import { exercise, exercise_session, session } from "@/lib/schema";
 import { db } from "@/lib/turso";
-import { eq, isNull, not, notExists, notInArray } from "drizzle-orm";
+import { notEqual } from "assert";
+import {
+  and,
+  eq,
+  inArray,
+  isNull,
+  ne,
+  not,
+  notExists,
+  notInArray,
+  or,
+  sql,
+} from "drizzle-orm";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const result = await db
-    .select()
-    .from(exercise_session)
-    .innerJoin(exercise, eq(exercise_session.exercise_id, exercise.id))
-    .all();
+  const sessionId = parseInt(params.id, 10);
 
-  // const other = await db
-  //   .select()
-  //   .from(exercise_session)
-  //   .innerJoin(exercise, eq(exercise_session.exercise_id, exercise.id))
-  //   .all();
-
-  const other = await db
+  const existing = await db
     .select()
     .from(exercise_session)
     .rightJoin(exercise, eq(exercise_session.exercise_id, exercise.id))
-    .where(isNull(exercise_session.exercise_id))
+    .where(eq(exercise_session.session_id, sessionId))
+    .all();
+
+  const exerciseIds = await db
+    .select()
+    .from(exercise_session)
+    .where(eq(exercise_session.session_id, sessionId))
+    .all()
+    .then((data) =>
+      data.length !== 0
+        ? data.map((e) => (e.exercise_id === null ? -1 : e.exercise_id))
+        : [-1]
+    );
+
+  const other = await db
+    .select()
+    .from(exercise)
+    .where(notInArray(exercise.id, exerciseIds))
     .all();
 
   return (
     <div className="p-20 text-center">
-      <h1 className="text-5xl font-bold">{params.id}</h1>
-
+      <h1 className="text-5xl font-bold">{sessionId}</h1>
       <div className="grid grid-cols-6">
-        {result.map((e) => {
+        {existing.map((e) => {
           const { id, title, instructions, url } = e.exercise;
           return (
             <div
@@ -39,7 +57,7 @@ export default async function Page({ params }: { params: { id: string } }) {
               <h2 className="text-xl font-bold">{title}</h2>
               <p className="text-sm">{instructions}</p>
               <div className="flex gap-2">
-                <RemoveButton ex={id} sesh={parseInt(params.id, 10)} />
+                <RemoveButton ex={id} sesh={sessionId} />
               </div>
             </div>
           );
@@ -48,7 +66,7 @@ export default async function Page({ params }: { params: { id: string } }) {
 
       <div className="grid grid-cols-6">
         {other.map((e) => {
-          const { id, title, instructions, url } = e.exercise;
+          const { id, title, instructions, url } = e;
           return (
             <div
               key={id}
@@ -57,7 +75,7 @@ export default async function Page({ params }: { params: { id: string } }) {
               <h2 className="text-xl font-bold">{title}</h2>
               <p className="text-sm">{instructions}</p>
               <div className="flex gap-2">
-                <AddButton ex={id} sesh={parseInt(params.id, 10)} />
+                <AddButton ex={id} sesh={sessionId} />
               </div>
             </div>
           );
