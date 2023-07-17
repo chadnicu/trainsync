b35b77c0484b406d5b4bb5de2369e6f21d908b91
nicu420 <ontanicolae15@gmail.com>
2023-07-17 22:35:22 +0300

    d

--- components/AddButtonn.tsx
+++ components/AddButtonn.tsx
@@ -4,7 +4,5 @@ import { addExerciseToSession } from "@/app/actions";
 import { Button } from "./ui/button";
 
 export default function AddButton({ ex, sesh }: { ex: number; sesh: number }) {
-  return (
-    <Button onClick={() => addExerciseToSession(ex, sesh)}>Add</Button>
-  );
+  return <Button onClick={() => addExerciseToSession(ex, sesh)}>Add</Button>;
 }
