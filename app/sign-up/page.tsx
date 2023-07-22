be198c5ebda37aed6886197376ce1229af1c3662
nicu420 <ontanicolae15@gmail.com>
2023-07-22 22:55:01 +0300

    deployment fix

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { authElements } from "@/lib/clerk-styles";
+import { buttonVariants } from "@/components/ui/button";
 import { SignUp } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
@@ -13,7 +13,20 @@ export default function SignUpPage() {
       <SignUp
         appearance={{
           baseTheme: theme === "light" ? undefined : dark,
-          elements: authElements
+          elements: {
+            formButtonPrimary: buttonVariants(),
+            formFieldLabel: "text-foreground",
+            formField: "gap-1",
+            card: "bg-background dark:border-zinc-700",
+            socialButtonsIconButton__google:
+              "border border-zinc-200 dark:border-zinc-700",
+            socialButtonsIconButton__github:
+              "border border-zinc-200 dark:border-zinc-700",
+            socialButtonsIconButton__discord:
+              "border border-zinc-200 dark:border-zinc-700",
+            dividerLine: "bg-zinc-200 dark:bg-zinc-700",
+            footerActionLink: "text-foreground hover:text-foreground",
+          },
         }}
       />
     </div>
