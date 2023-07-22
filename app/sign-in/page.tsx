6d75a565108f2854df9acb8d732c24559cd8f961
nicu420 <ontanicolae15@gmail.com>
2023-07-22 23:14:18 +0300

    no idea why styles dont work on sign-up

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,40 +1,35 @@
-"use client";
-
 import { buttonVariants } from "@/components/ui/button";
 import { SignIn } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import { useTheme } from "next-themes";
 
 export default function SignInPage() {
-  const { theme } = useTheme();
-
   return (
     <div className="flex w-full justify-center ">
       <SignIn
         appearance={{
-          baseTheme: theme === "light" ? undefined : dark,
-          elements: {
-            headerTitle: "text-foreground",
-            headerSubtitle: "text-foreground",
-            dividerText: "text-zinc-500",
-            formButtonPrimary: buttonVariants({ variant: "secondary" }),
-            formFieldLabel: "text-foreground",
-            socialButtonsProviderIcon__github: "brightness-50 dark:brightness-[10]",
-            formFieldInput: "bg-transparent border border-zinc-300 dark:border-zinc-700",
-            footerActionText: "text-zinc-500",
-            formField: "gap-1",
-            card: "bg-background dark:border-zinc-700",
-            socialButtonsIconButton__google:
-              "border border-zinc-200 dark:border-zinc-700",
-            socialButtonsIconButton__github:
-              "border border-zinc-200 dark:border-zinc-700",
-            socialButtonsIconButton__discord:
-              "border border-zinc-200 dark:border-zinc-700",
-            dividerLine: "bg-zinc-200 dark:bg-zinc-700",
-            footerActionLink: "text-foreground hover:text-foreground",
-          },
+          elements: appearance,
         }}
       />
     </div>
   );
 }
+
+export const appearance = {
+  headerTitle: "text-foreground",
+  headerSubtitle: "text-foreground",
+  dividerText: "text-zinc-500",
+  formButtonPrimary: buttonVariants({ variant: "secondary" }),
+  formFieldLabel: "text-foreground",
+  socialButtonsProviderIcon__github: "brightness-50 dark:brightness-[10]",
+  formFieldInput: "bg-transparent border border-zinc-300 dark:border-zinc-700",
+  footerActionText: "text-zinc-500",
+  formField: "gap-1",
+  card: "bg-background dark:border-zinc-700",
+  socialButtonsIconButton__google:
+    "border border-zinc-200 dark:border-zinc-700",
+  socialButtonsIconButton__github:
+    "border border-zinc-200 dark:border-zinc-700",
+  socialButtonsIconButton__discord:
+    "border border-zinc-200 dark:border-zinc-700",
+  dividerLine: "bg-zinc-200 dark:bg-zinc-700",
+  footerActionLink: "text-foreground hover:text-foreground",
+};
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,34 +1,12 @@
-"use client";
-
-import { buttonVariants } from "@/components/ui/button";
 import { SignUp } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
-import { useTheme } from "next-themes";
+import { appearance } from "../sign-in/page";
 
 export default function SignUpPage() {
-  const { theme } = useTheme();
-
   return (
     <div className="flex w-full justify-center">
       <SignUp
         appearance={{
-          baseTheme: theme === "light" ? undefined : dark,
-          elements: {
-            headerTitle: "text-foreground",
-            headerSubtitle: "text-foreground",
-            formButtonPrimary: buttonVariants(),
-            formFieldLabel: "text-foreground",
-            formField: "gap-1",
-            card: "bg-background dark:border-zinc-700",
-            socialButtonsIconButton__google:
-              "border border-zinc-200 dark:border-zinc-700",
-            socialButtonsIconButton__github:
-              "border border-zinc-200 dark:border-zinc-700",
-            socialButtonsIconButton__discord:
-              "border border-zinc-200 dark:border-zinc-700",
-            dividerLine: "bg-zinc-200 dark:bg-zinc-700",
-            footerActionLink: "text-foreground hover:text-foreground",
-          },
+          elements: appearance
         }}
       />
     </div>
