191c519a992e613b5a5407574f0f3e439ceb4916
nicu <ontanicolae15@gmail.com>
2023-09-08 11:47:54 +0300

    updated homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import { Github, GithubIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Page() {
   return (
-    <h1 className="mb-20 break-words text-center text-5xl font-bold sm:mb-10">
-      Gymlogger by nicu420
+    <h1 className="mb-20 break-words text-center text-5xl font-bold sm:mb-10 md:text-6xl">
+      Workout Tracker
+      <Link
+        href={"https://github.com/iusedebian/workout-tracker"}
+        className="flex items-center justify-center gap-1 text-purple-300"
+        target="/blank"
+      >
+        <span className="text-primary">by</span>@iusedebian
+        <GithubIcon size={50} />
+      </Link>
     </h1>
   );
 }
