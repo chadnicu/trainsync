525695b5aabb8e1d5f5b518a1a407b9980ded19a
nicu <ontanicolae15@gmail.com>
2023-12-05 19:58:20 +0200

    add tailwind animation back

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,14 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <NextThemesProvider enableSystem attribute="class" defaultTheme="system">
+    <ThemeProvider enableSystem attribute="class" defaultTheme="system">
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-    </NextThemesProvider>
+    </ThemeProvider>
   );
 }
