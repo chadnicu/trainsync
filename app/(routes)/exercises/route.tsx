8f219db42b6146fa6a1259715d8fdae4dd916dfb
nicu <ontanicolae15@gmail.com>
2023-12-11 12:40:33 +0200

    checking somethign

--- app/(routes)/exercises/route.tsx
+++ app/(routes)/exercises/route.tsx
@@ -1,10 +1,9 @@
 import { ImageResponse } from "next/og";
-import Page from "../../page";
 
 export const runtime = "edge";
 
 export async function GET() {
-  return new ImageResponse(<Page />, {
+  return new ImageResponse(<div></div>, {
     width: 1200,
     height: 600,
   });
