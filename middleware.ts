b32aeb39172e73982b982981371135cf24e1472d
nicu <ontanicolae15@gmail.com>
2023-12-11 18:34:30 +0200

    fixing metadata

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/opengraph-image", "/twitter-image"],
 });
 
 export const config = {
