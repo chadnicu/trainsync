8e52c138ca2d234aef7ab18443e2fdb0e910999c
nicu <ontanicolae15@gmail.com>
2023-12-11 12:57:37 +0200

    might be onto something

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
   applicationName: "TrainSync",
   authors: { name: "iusedebian", url: "https://twitter.com/iusedebian" },
   creator: "Onța Nicolae",
+  openGraph: {
+    images: ["/api"],
+  },
 };
 
 const inter = Inter({ subsets: ["latin"] });
