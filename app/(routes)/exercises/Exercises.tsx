5ab62867bfd19caf20e573355b8f919c7f9a5025
nicu <ontanicolae15@gmail.com>
2023-12-30 15:44:26 +0200

    syntax error, forgot to return

--- app/(routes)/exercises/Exercises.tsx
+++ app/(routes)/exercises/Exercises.tsx
@@ -12,13 +12,14 @@ export default function Exercises() {
     initialData: [],
   });
 
-  if (!isFetched) {
-    <div className="grid grid-cols-1 items-start gap-10 lg:grid-cols-2 xl:grid-cols-3">
-      {Array.from({ length: 10 }, (_, i) => (
-        <ExerciseSkeleton key={i} />
-      ))}
-    </div>;
-  }
+  if (!isFetched)
+    return (
+      <div className="grid grid-cols-1 items-start gap-10 lg:grid-cols-2 xl:grid-cols-3">
+        {Array.from({ length: 10 }, (_, i) => (
+          <ExerciseSkeleton key={i} />
+        ))}
+      </div>
+    );
 
   return (
     <div className="grid grid-cols-1 items-start gap-10 lg:grid-cols-2 xl:grid-cols-3">
