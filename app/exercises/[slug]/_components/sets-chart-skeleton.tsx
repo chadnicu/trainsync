596a06d4e963fa75ebc94ffacfa6433e88b9c52f
nicu <ontanicolae15@gmail.com>
2024-03-22 20:24:28 +0200

    make loading page in exercises/slug responsive

--- app/exercises/[slug]/_components/sets-chart-skeleton.tsx
+++ app/exercises/[slug]/_components/sets-chart-skeleton.tsx
@@ -9,12 +9,12 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function SetsChartSkeleton() {
   return (
-    <Card className="w-fit mx-auto">
+    <Card className="w-full mx-auto sm:max-w-[472px]">
       <CardHeader className="text-left">
         <CardTitle>
           <Skeleton className="w-[60%] h-5" />
         </CardTitle>
-        <CardDescription className="w-[425px]">
+        <CardDescription className="w-full">
           <Skeleton className="h-5 w-full" />
         </CardDescription>
       </CardHeader>
