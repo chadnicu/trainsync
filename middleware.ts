2447345917d2c268661f43f93af543d3da371543
nicu <ontanicolae15@gmail.com>
2024-04-26 09:16:52 +0300

    trying deprecated clerk middleware to fix google error
    Failed: Blocked due to unauthorized request (401)

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,20 @@
-import { authMiddleware } from "@clerk/nextjs";
+// import { authMiddleware } from "@clerk/nextjs";
 
-export default authMiddleware({
-  publicRoutes: ["/", "/faq", "/opengraph-image", "/twitter-image"],
+// export default authMiddleware({
+//   publicRoutes: ["/", "/faq", "/opengraph-image", "/twitter-image"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from 'next/server'
+
+export default withClerkMiddleware((req: NextRequest) => {
+  return NextResponse.next();
 });
 
-export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+// Stop Middleware running on static files
+export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',};
