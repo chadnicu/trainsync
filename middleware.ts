0a2d92f69f8a6dd102831c83b5ba333ddaefd25b
nicu <ontanicolae15@gmail.com>
2024-04-26 09:20:51 +0300

    old middleware doesnt protect any page

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,9 @@
-// import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware } from "@clerk/nextjs";
 
-// export default authMiddleware({
-//   publicRoutes: ["/", "/faq", "/opengraph-image", "/twitter-image"],
-// });
-
-// export const config = {
-//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-import type { NextRequest } from 'next/server'
-
-export default withClerkMiddleware((req: NextRequest) => {
-  return NextResponse.next();
+export default authMiddleware({
+  publicRoutes: ["/", "/faq", "/opengraph-image", "/twitter-image"],
 });
 
-// Stop Middleware running on static files
-export const config = { matcher:  '/((?!_next/image|_next/static|favicon.ico).*)',};
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
