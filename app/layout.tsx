e16427fc4e709e0d7eccb7ad551c8842a3ca88cc
nicu <ontanicolae15@gmail.com>
2024-05-10 21:57:50 +0300

    trying some seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,64 @@ export const metadata: Metadata = {
   title: "TrainSync - The Web Based Workout Tracker",
   description:
     "TrainSync is a the simple, functional and completely free workout tracker for the web, designed to help you achieve your lifting goals more efficiently.",
+  generator: "Next.js",
+  applicationName: "TrainSync",
+  referrer: "origin-when-cross-origin",
+  keywords: [
+    "TrainSync",
+    "Workout Tracker",
+    "Workout",
+    "Tracker",
+    "Fitness",
+    "Logger",
+    "Sets",
+    "Reps",
+    "Exercises",
+    "App",
+    "Web",
+    "Train",
+    "Sync",
+  ],
+  authors: [{ name: "Nicu", url: "https://twitter.com/chadnicu" }],
+  creator: "Onța Nicolae",
+  publisher: "Onța Nicolae",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+  openGraph: {
+    title: "TrainSync",
+    description:
+      "TrainSync is a the simple, functional and completely free workout tracker for the web, designed to help you achieve your lifting goals more efficiently.",
+    url: "https://trainsync.online",
+    siteName: "TrainSync",
+    images: [
+      {
+        url: "https://www.trainsync.online/opengraph-image", // Must be an absolute URL
+        width: 1800,
+        height: 1600,
+      },
+      {
+        url: "https://www.trainsync.online/opengraph-image", // Must be an absolute URL
+        width: 800,
+        height: 600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TrainSync",
+    description:
+      "TrainSync is a the simple, functional and completely free workout tracker for the web, designed to help you achieve your lifting goals more efficiently",
+    siteId: "1132731405539188736",
+    creator: "@chadnicu",
+    creatorId: "1132731405539188736",
+    images: ["https://trainsync.online/opengraph-image"], // Must be an absolute URL
+  },
 };
 
 export default function RootLayout({
