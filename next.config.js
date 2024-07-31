0ecdbc762f202e9ee0309ed60985ca0684bbe14d
nicu <ontanicolae15@gmail.com>
2024-07-31 15:29:37 +0300

    doesnt work

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
-// const withPWA = require("@ducanh2912/next-pwa").default({
-//     dest: "public",
-//     cacheOnFrontEndNav: true,
-//     aggressiveFrontEndNavCaching: true,
-//     reloadOnOnline: true,
-//     swcMinify: true,
-//     disable: process.env.NODE_ENV === "development",
-//     workboxOptions: {
-//         disableDevLogs: true,
-//     },
-//     // ... other options you like
-// });
+const withPWA = require("@ducanh2912/next-pwa").default({
+    dest: "public",
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    // disable: process.env.NODE_ENV === "development",
+    workboxOptions: {
+        disableDevLogs: true,
+    },
+    // ... other options you like
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -28,12 +28,12 @@ const nextConfig = {
     }
 }
 
-const withPWA = require("next-pwa")({
-    dest: "public", // Destination directory for the PWA files
-    disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-    register: true, // Register the PWA service worker
-    skipWaiting: true, // Skip waiting for service worker activation
-});
+// const withPWA = require("next-pwa")({
+//     dest: "public", // Destination directory for the PWA files
+//     // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+//     register: true, // Register the PWA service worker
+//     skipWaiting: true, // Skip waiting for service worker activation
+// });
 
 // Export the combined configuration for Next.js with PWA support
 module.exports = withPWA(nextConfig);
